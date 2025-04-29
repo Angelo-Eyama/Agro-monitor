aa75cfddbaa2f594e733cd77a4e34eccbfb53ed7
Angelo <111171031+Angelo-Eyama@users.noreply.github.com>
2025-04-29 10:21:23 +0200

    Add config file with env variables

---
+++ frontend/src/config.js
@@ -0,0 +1,6 @@
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
+export const OPENAPI_APIKEY = import.meta.env.VITE_OPENAPI_APIKEY;
+export const OPENAI_APIKEY = import.meta.env.VITE_OPENAI_APIKEY;
+export const OPENAI_BASE_URL = import.meta.env.VITE_OPENAI_BASE_URL;
+export const OPENAI_MODEL = import.meta.env.VITE_OPENAI_MODEL;
+export const OPENWEATHERMAP_APIKEY = import.meta.env.VITE_OPENWEATHERMAP_APIKEY;
