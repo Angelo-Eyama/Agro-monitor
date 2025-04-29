4f8d8ba30902956882cfac957380b084b0793ed8
Angelo <111171031+Angelo-Eyama@users.noreply.github.com>
2025-04-29 10:42:37 +0200

    Update CORS config to accept specific origins

--- backend/cors.py
+++ backend/cors.py
@@ -3,7 +3,10 @@ from fastapi.middleware.cors import CORSMiddleware
 
 def configure_cors(app):
     origins = [
-        "*",
+        "http://localhost:8000", # Backend
+        "http://localhost:5173", # Frontend (Vite)
+        "http://localhost:8080", # Frontend (Vue CLI)
+        "*",    #⚠️ Allow all origins for development purposes
     ]
 
     app.add_middleware(
